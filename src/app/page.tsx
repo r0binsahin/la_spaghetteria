a6736ae4b4b8317560ba740ee3b41f040e4df072
r0binsahin <robin.sahin88@gmail.com>
2024-08-12 14:29:05 +0200

    setting date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import styles from './page.module.css';
 import * as Components from '../components/index';
+import { useState } from 'react';
 
 export default function Home() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <main className={styles.main}>
-      <Components.Calendar />
+      <Components.Calendar date={date} setDate={setDate} />
     </main>
   );
 }
--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,14 +3,24 @@
 import ReactCalendar from 'react-calendar';
 import '../styles/react-calender.css';
 
-export const Calendar = () => {
+interface CalendarProps {
+  date: Date;
+  setDate: (date: Date) => void;
+}
+
+export const Calendar = ({ date, setDate }: CalendarProps) => {
   const now = new Date();
+
   return (
     <>
       <ReactCalendar
         minDate={now}
         view='month'
         className='REACT-CALENDAR p-2'
+        //@ts-ignore
+        onChange={setDate}
+        value={date}
+        locale='sv-SE'
       />
     </>
   );
