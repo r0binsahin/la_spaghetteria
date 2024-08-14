4b74e3ebccf04182111e1bd760326037e4881380
r0binsahin <robin.sahin88@gmail.com>
2024-08-14 09:57:07 +0200

    link added to book a table

--- src/components/slide-show/slide-show.tsx
+++ src/components/slide-show/slide-show.tsx
@@ -1,10 +1,17 @@
+import Link from 'next/link';
 import './slide-show.css';
 
 export const SlideShow = () => {
   return (
     <div className='slide-container'>
       <div className='page-title'>
-        <h1>Benvenuti a La Spaghetteria </h1>
+        <div className='welcome'>
+          <h1>Benvenuti a La Spaghetteria </h1>
+        </div>
+
+        <Link href={'/book-table'} className='book-table-btn'>
+          Book a table
+        </Link>
       </div>
       <div className='slider'>
         <div className='slides'>
@@ -15,7 +22,7 @@ export const SlideShow = () => {
             <img src='https://images.unsplash.com/photo-1534425582704-65e021820688?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
           </div>
           <div className='slide'>
-            <img src='https://images.unsplash.com/photo-1556761223-4c4282c73f77?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+            <img src='https://images.unsplash.com/photo-1556442281-77c90134c61f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
           </div>
           <div className='slide'>
             <img src='https://images.unsplash.com/photo-1562255604-4aec5a149a06?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
