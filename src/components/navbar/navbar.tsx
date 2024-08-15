146d0b61d36d3a5209f6b807455b9806410f02a6
r0binsahin <robin.sahin88@gmail.com>
2024-08-15 18:52:08 +0200

    navbar done

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import './navbar.css';
 import { Dancing_Script } from 'next/font/google';
 import { FaCircleUser } from 'react-icons/fa6';
 
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
 import { Logo } from '@/assets/logo';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 const dancing = Dancing_Script({ subsets: ['latin'] });
 
 export const Navbar = () => {
+  const pathname = usePathname();
+  const isAdminPage = pathname === '/admin-page';
   return (
     <nav>
       <div className='topnav'>
@@ -18,9 +25,24 @@ export const Navbar = () => {
         </Link>
       </div>
 
-      <Link href='/admin-page' className='admin-page-link'>
-        <FaCircleUser />
-      </Link>
+      {isAdminPage ? (
+        <div className='login-btn-wrapper'>
+          <SignedOut>
+            <SignInButton>
+              <span className='border-2 border-black px-4 rounded-lg text-base cursor-pointer font-normal'>
+                <FaCircleUser className='admin-user-login' />
+              </span>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>{' '}
+        </div>
+      ) : (
+        <Link href='/admin-page' className='admin-page-link'>
+          <FaCircleUser />
+        </Link>
+      )}
     </nav>
   );
 };
